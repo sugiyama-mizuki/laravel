b0239325be7813ee762237ff050b5b650c13fd75
sugiyama-mizuki <mk7gac4@gmail.com>
2022-03-18 10:46:20 +0900

    laravel2022/03/18 0012

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -9,4 +9,16 @@ class Person extends Model
     public function getData(){
         return $this->id . ': ' . $this->name . '(' . $this->age . ')';
     }
+
+    public function scopeNameEquery($query, $str){
+        return $query->where('name', $str);
+    } 
+
+    public function scopeAgeGreaterThan($query, $n){
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n){
+        return $query->where('age', '<=', $n);
+    }
 }
